b758f3f14fec7c595c589f1e4996db082f213d14
Juan Sebastian Rodriguez <jsrb48@gmail.com>
2022-11-08 21:13:55 -0500

    Conexion a la base de datos local Back

--- back/app.js
+++ back/app.js
@@ -1,7 +1,18 @@
     const express = require("express");
     const app = express();
-    app.use(express.json())
+    const errorMiddleware = require("./middleware/errors")
+
+    app.use(express.json());
+    
     const productos = require("./routes/products")
+    //const usuarios = require("./routes/auth")
+
     app.use('/api',productos)
+    //app.use('/api',usuarios)
+
+    
+
+    //MiddleWare para manejar errores
+    app.use(errorMiddleware)
 
     module.exports = app    
--- back/server.js
+++ back/server.js
@@ -1,7 +1,10 @@
 const app = require("./app")
 
 const dotenv=require("dotenv");
+const connectDatabase = require("./config/database");
 dotenv.config({path: 'back/config/config.env'})
+
+connectDatabase();
 
 const server=app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
